6ee02d7b4008d2afc2a0373afe71e2b27cf47357
Mibin Thomas <mibinthomas22@gmail.com>
2026-04-26 19:53:39 +0400

    Update Behance URL

--- src/components/SocialSidebar.tsx
+++ src/components/SocialSidebar.tsx
@@ -6,7 +6,7 @@ export function SocialSidebar() {
       <div className="social-links">
         <a href="https://www.instagram.com/mibz.t/" target="_blank" rel="noopener noreferrer">Instagram</a>
         <a href="https://www.linkedin.com/in/mibin-thomas/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-        <a href="#" target="_blank" rel="noopener noreferrer">Behance</a>
+        <a href="https://www.behance.net/mibinthomas1" target="_blank" rel="noopener noreferrer">Behance</a>
       </div>
       <div className="line-separator"></div>
       <div className="scroll-indicator">Scroll</div>
